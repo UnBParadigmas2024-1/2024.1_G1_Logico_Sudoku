e06f14a98c5def2dc5ccaddfb3639bb51d82ebaf
Mateus <mateus.frauzino@gmail.com>
2024-07-15 22:27:57 -0300

    create server.pl

---
+++ api/server.pl
@@ -0,0 +1,18 @@
+% server.pl
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_json)).
+:- use_module(library(http/http_files)).
+
+:- http_handler(root(.), http_reply_from_files('.', []), [prefix]).
+:- http_handler(root(api/solve), solve_sudoku_handler, []).
+
+server(Port) :-
+    http_server(http_dispatch, [port(Port)]).
+
+solve_sudoku_handler(Request) :-
+    http_read_json_dict(Request, DictIn),
+    sudoku:solve_sudoku(DictIn.board, Solution),
+    reply_json_dict(_{solution: Solution}).
+
+:- initialization(server(8080)).
