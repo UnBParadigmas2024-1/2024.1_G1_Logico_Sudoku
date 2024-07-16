bc3271ace51f4ab8733f0dd4cf6b297372f2806d
phrezende-eng <ph.hr.001@gmail.com>
2024-07-15 23:31:03 -0300

    feat: verifica elemento na linha
    Co-authored-by: ltakehana<leonardotakehana@gmail.com>
    Co-authored-by: PauloAbiAcl<pv.silva23@hotmail.com>

--- prolog/main.pl
+++ prolog/main.pl
@@ -1,4 +1,3 @@
-
 create_row(N, Elem, Row) :-
     length(Row, N),
     maplist(=(Elem), Row).
@@ -8,11 +7,18 @@ create_matrix(N, Elem, Matrix) :-
     maplist(create_row(N, Elem), Matrix).
 
 init_matrix(Matrix) :-
-    create_matrix(9, 0, Matrix).
+    create_matrix(9, 2, Matrix).
 
 element_at(Matrix, Row, Col, Element) :-
     nth1(Row, Matrix, MatrixRow),
     nth1(Col, MatrixRow, Element).
+
+element_exists(Element, List) :-
+    member(Element, List).
+
+verify_in_row(Element, Matrix, Row) :-
+    nth1(Row, Matrix, MatrixRow),
+    element_exists(Element, MatrixRow).
 
 
 :- initialization(main).
@@ -25,5 +31,9 @@ main :-
     % Acessa o elemento na posição (2, 3)
     element_at(Matrix, 2, 3, Element),
     format('Elemento na posição (2, 3): ~w~n', [Element]),
+
+    (verify_in_row(0, Matrix, 2) -> format('Elemento 0 encontrado na linha 2~n') ; format('Elemento 0 não encontrado na linha 2~n')),
+
+   
 
     halt.
