1b99583a60cea26b68fee2ff8e3c09136307621a
Bernardo Pissutti <bernardopissutti@gmail.com>
2024-07-17 16:22:03 -0300

    Adiciona handler lógica para vidas no jogo
    Co-authored-by: diogjunior100 <diogjunior10071@gmail.com>

--- api/server.pl
+++ api/server.pl
@@ -2,36 +2,45 @@
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_json)).
 :- use_module(library(http/http_files)).
+:- use_module(library(nb_set)).
 
 :- use_module(main).
 
 server(Port) :-
     http_server(http_dispatch, [port(Port)]).
 
-:- http_handler(root(.), http_reply_from_files('.', []), [prefix]).
-
 :- http_handler(root('sudoku/start'), start_sudoku_handler, [method(get)]).
 :- http_handler(root('sudoku/solve'), solve_sudoku_handler, [method(post)]).
 :- http_handler(root('sudoku/get-lives'), get_lives_handler, [method(get)]).
 :- http_handler(root('sudoku/get-help'), get_help_handler, [method(get)]).
 
-
 start_sudoku_handler(_Request) :-
     main:init_matrix(Matrix1),
     main:fill_matrix(Matrix1, Matrix2),
+    nb_setval(sudoku_matrix, Matrix2),
+    nb_setval(lives, 3),
     reply_json_dict(_{puzzle: Matrix2}).
 
 solve_sudoku_handler(Request) :-
     http_read_json_dict(Request, DictIn),
-    main:compare_9x9_matrices(DictIn.puzzle, Solution),
-    reply_json_dict(_{solution: Solution}).
+    nb_getval(sudoku_matrix, StoredMatrix),
+    ( main:compare_9x9_matrices(DictIn.puzzle, StoredMatrix)
+    ->  reply_json_dict(_{solution: StoredMatrix})
+    ;   nb_getval(lives, Lives),
+        NewLives is Lives - 1,
+        nb_setval(lives, NewLives),
+        reply_json_dict(_{error: 'Solução incorreta', vidas: NewLives})
+    ).
 
-% get_lives_handler(_Request) :-
-    % main:get_lives(Lives),
-    % reply_json_dict(_{vidas: Lives}).
+get_lives_handler(_Request) :-
+    nb_getval(lives, Lives),
+    reply_json_dict(_{vidas: Lives}).
 
-% get_help_handler(_Request) :- 
-%     main:get_help(Help),
-%     reply_json_dict(_{mensagem: Help})
+% get_help_handler(_Request) :-
+%     % Substitua pela sua lógica de ajuda
+%     reply_json_dict(_{mensagem: "Aqui está a sua ajuda!"}).
 
 :- initialization(server(8080)).
+
+
+
